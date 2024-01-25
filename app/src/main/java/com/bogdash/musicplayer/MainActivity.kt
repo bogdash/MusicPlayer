dc7f7b0e3bf7db38fff1266f09cf393f454c7c11
Anastasia <ms.anastasia.bogdanova@gmail.com>
2024-01-25 12:38:41 +0300

    Add ViewBinding support

--- app/src/main/java/com/bogdash/musicplayer/MainActivity.kt
+++ app/src/main/java/com/bogdash/musicplayer/MainActivity.kt
@@ -2,10 +2,13 @@ package com.bogdash.musicplayer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bogdash.musicplayer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
